53d4301a9c13b673aaa458667e784d6525c1e876
GunGuyDude <kyleleungsauhay@gmail.com>
2025-04-26 18:01:26 +0100

    Added MLAgents libraries

---
+++ Assets/Scripts/AI/BuildingAI.cs
@@ -0,0 +1,11 @@
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class BuildingAI : Agent
+{
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AI/UnitAI.cs
@@ -0,0 +1,11 @@
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class UnitAI : Agent
+{
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+
+    }
+}
